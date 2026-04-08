3c8a18d176d1c4c58deb00d0d8763400571358ce
tig <leehjpp4@gmail.com>
2026-04-08 17:42:51 +0900

    init

--- src/main/java/kr/co/gitops/HomeController.java
+++ src/main/java/kr/co/gitops/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//asdasdasd
+//asdasdasddasdasdsa
 @RestController
 public class HomeController {
 	
