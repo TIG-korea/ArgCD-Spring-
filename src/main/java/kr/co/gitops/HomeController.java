84815be43f5744bd54209148646ea56cd6695a95
tig <leehjpp4@gmail.com>
2026-04-08 17:56:44 +0900

    test

--- src/main/java/kr/co/gitops/HomeController.java
+++ src/main/java/kr/co/gitops/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//asdasdasddasdasdsa
+//asdasdasddasdasdsaasdasd
 @RestController
 public class HomeController {
 	
